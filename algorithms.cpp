623ff1dbfe348b40a863dec467373f4d471d0c27
Jeremy O.A Boateng <160321903+nojetlag7@users.noreply.github.com>
2024-06-08 00:46:59 +0000

    Update algorithms.cpp

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,3 +1,249 @@
+//REFERENCES
+/*
+    string food = "Pizza" // food variable
+    string &meal = food // reference to food
+
+    hence,
+    cout << food << endl
+    cout << meal << "\n"
+    both would output Pizza
+*/
+// MEMORY ADDRESS
+/*
+When a variable is created in C++, a memory address is assigned to the variable. 
+And when we assign a value to the variable,
+it is stored in this memory address.
+To access it, use the & operator, and the result will represent
+where the variable is stored:
+
+string food = "Pizza";
+
+cout << &food; // Outputs 0x6dfed4
+*/
+//POINTERS
+/*
+A pointer is a variable that stores the memory address as its value.
+A pointer variable points to a data type (like int or string) of the same type, and is created with the * operator. 
+The address of the variable you're working with is assigned to the pointer:
+
+string food = "Pizza";  // A food variable of type string
+string* ptr = &food;    // A pointer variable, with the name ptr, that stores the address of food
+
+// Output the value of food (Pizza)
+cout << food << "\n";
+
+// Output the memory address of food (0x6dfed4)
+cout << &food << "\n";
+
+// Output the memory address of food with the pointer (0x6dfed4)
+cout << ptr << "\n";
+cout << *ptr 
+outputs the content of what is stored at the memory address
+which is pizza.
+*/
+//to set a function with a default parameter value, use =. eg
+// void myFunction(strinf country = " Norway")
+//FUNCTIONS
+/*
+the 'void' keyword is for functions that do not return any value
+but if we want to return values, we can use a data type such as
+int, string, double, etc.
+
+int myFunction(int x){
+    return 2 * x;
+}
+int main(){
+    cout << myFucntion(3); --> outputs 6(2 *3)
+    int twice = myFunction(9) stores the value of 2 * 9 in 'twice'
+    return 0;
+}
+*/
+// look deeper into recursion(calling a function within itself)
+//RECURSION
+/*
+int sum(int k) {
+  if (k > 0) {
+    return k + sum(k - 1);
+  } else {
+    return 0;
+  }
+}
+
+int main() {
+  int result = sum(10);
+  cout << result;
+  return 0;
+}
+
+Example Explained
+When the sum() function is called, it adds parameter k to the 
+sum of all numbers smaller than k and returns the result.
+ When k becomes 0, the function just returns 0. When running,
+the program follows these steps:
+
+10 + sum(9)
+10 + ( 9 + sum(8) )
+10 + ( 9 + ( 8 + sum(7) ) )
+...
+10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 + sum(0)
+10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 + 0
+Since the function does not call itself when k is 0, the program stops there and returns the result.
+*/
+
+//CLASSES AND OBJECTS.
+/*
+a class is a template for objects, an object is an instance of a class.
+eg.
+class --> CAR
+objects --> Volvo Audi Dodge
+*/
+/* #include <iostream>
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    std::string myString;  // Attribute (string variable)
+};
+class Cars{
+  public:
+    int manuDate;
+    std::string model;
+};
+
+int main() {
+  MyClass myObj;  // Create an object of MyClass
+
+  // Access attributes and set values
+  myObj.myNum = 15; 
+  myObj.myString = "Some text";
+
+  // Print attribute values
+  std::cout << myObj.myNum << "\n";
+  std::cout << myObj.myString;
+
+  Cars Benz;
+  Benz.manuDate = 2019;
+  Benz.model = "Gx99";
+//Below is a code snippet for a class of cars.
+/*
+// Create a Car class with some attributes
+class Car {
+  public:
+    string brand;   
+    string model;
+    int year;
+};
+
+int main() {
+  // Create an object of Car
+  Car carObj1;
+  carObj1.brand = "BMW";
+  carObj1.model = "X5";
+  carObj1.year = 1999;
+
+  // Create another object of Car
+  Car carObj2;
+  carObj2.brand = "Ford";
+  carObj2.model = "Mustang";
+  carObj2.year = 1969;
+
+  // Print attribute values
+  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+  return 0;
+}
+*/
+
+//CLASS METHODS
+/*
+methods are functions that belong to a class
+To define a function outside the class definition,
+you have to declare it inside the class and 
+then define it outside of the class. 
+This is done by specifiying the name of the class,
+followed the scope resolution :: operator,
+followed by the name of the function:
+
+class MyClass {        // The class
+  public:              // Access specifier
+    void myMethod();   // Method/function declaration
+};
+
+// Method/function definition outside the class
+void MyClass::myMethod() {
+  cout << "Hello World!";
+}
+
+int main() {
+  MyClass myObj;     // Create an object of MyClass
+  myObj.myMethod();  // Call the method
+  return 0;
+}
+*/
+
+//GENERATING RANDOM NUMBERS
+/*
+
+for generating random numbers we can use the rand() function
+which takes no argument.
+The rand() function returns a pseudo-random number between 0 and RAND_MAX.
+RAND_MAX is a constant defined in cstdlib.
+
+but to get truly random values,
+we need to use the srand() function.
+srand() takes an unsigned int as an argument.
+
+but to change it each time,
+we can do this
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+int main(){
+
+  srand(time(NULL));
+  cout << (rand() % 10) +1 //gives a random number from 1 to 10
+  cout << (rand() % 100) +1 //gives a random number from 1 to 100
+  return 0;
+}
+
+
+
+*/
+
+//TIME COUNTER
+/* #include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    cout << "Please enter time in seconds: "; // user prompt
+    int input; // int for time input
+    cin >> input; // writes time into input var
+
+    // clock_t is a data type used to represent time in clock ticks
+    
+    clock_t start_time = clock(); // start time
+    int elapsed_seconds = 0; //intialise a variable to 0 to keep track of elapsed time in seconds
+
+    while (elapsed_seconds < input) // while prog reaches inputted time
+    {
+        system("cls"); // Clear All of the text(clears the console screen for windows)
+        //makes the timer output appear to update in place
+        clock_t current_time = clock();  // Update the current time (captures the current processor time)
+        elapsed_seconds = (current_time - start_time) / CLOCKS_PER_SEC; // Calculate elapsed time in seconds
+        //nb-- there are 1000 clock ticks in a second.
+        cout << "Seconds since started: " << elapsed_seconds << endl; // Print the time since the program started in seconds
+    }
+
+    cout << "Completed in " << input << " seconds" << endl; // completed message
+    system("pause");
+    return 0;
+} */
+
 // NB: can initialise variables by : int number = 5 or int number(5);
 //Question 1(JB AND SONS)
 /* #include <iostream>
@@ -708,6 +954,281 @@ void chooseWinner(char player, char comp){
             case 's': cout << "It was a draw!";
                 break;
         }
+    }
+}*/
+
+//GRADING SYSTEM?
+/* #include <iostream>
+
+using namespace std;
+int main(){
+
+    int grade;
+     cout << "Welcome. Kindly enter your grade(0-100): ";
+    cin >> grade;
+    do{
+    cout << "Kindly enter an appropriate grade(0-100): ";
+    cin >> grade;
+    }
+    while(grade > 100 || grade < 0);
+
+    if(grade >= 70){
+        cout << "You have a grade A";
+    }
+    else if(grade >=60){
+        cout << "You have a grade B";
+    }
+    else if( grade >= 50){
+        cout << "You have a grade C";
+    }
+    else{
+        cout << "You have a grade F";
+    }
+
+    return 0;
+}*/
+
+//X AND O :^( version 1
+/*
+#include <iostream>
+#include <ctime>
+using namespace std;
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player , char computer);
+bool checkTie(char *spaces);
+
+int main(){
+    char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' ',};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    drawBoard(spaces);
+
+    while(running){
+        playerMove(spaces, player);
+        drawBoard(spaces);
+        if(checkWinner(spaces,player,computer)){
+            running = false;
+            break;            
+        }
+        else if(checkTie(spaces)){
+            running = false;
+            break;
+        }
+
+        computerMove(spaces, computer);
+        drawBoard(spaces);
+        if(checkWinner(spaces,player,computer)){
+            running = false;
+            break;
+        }
+        else if(checkTie(spaces)){
+            running = false;
+            break;
+        }
+    }
+    return 0;
+}
+void drawBoard(char *spaces){
+    cout << '\n';
+    cout << "     |     |     " << '\n';
+    cout << " " << spaces[0] << "   | " << spaces[1] << "   |  " << spaces[2] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << " " << spaces[3] << "   | " << spaces[4] << "   |  " << spaces[5] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << " " << spaces[6] << "   | " << spaces[7] << "   |  " << spaces[8] << "  " << '\n';
+    cout << "     |     |     " << '\n';
+    cout << '\n';
+}
+void playerMove(char *spaces, char player){
+    int number;
+    do{
+        cout << "Enter a spot to place a marker(1-9): ";
+        cin >> number;
+        number--;
+        if(spaces[number] == ' '){
+            spaces[number] = player;
+            break;
+        }
+    } while(!number > 0 || !number < 8);
+};
+void computerMove(char *spaces, char computer){
+    int number;
+    srand(time(NULL));
+
+    while(true){
+        number = rand() % 9;
+        if(spaces[number] == ' '){
+            spaces[number] = computer;
+            break;
+        }
+    }
+};
+bool checkWinner(char *spaces,char player, char computer){
+    
+    if((spaces[0] != ' ') && (spaces[0] == spaces[1]) && (spaces[1] == spaces[2])){
+      spaces[0] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else if((spaces[3] != ' ') && (spaces[3] == spaces[4]) && (spaces[4] == spaces[5])){
+      spaces[3] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else if((spaces[6] != ' ') && (spaces[6] == spaces[7]) && (spaces[7] == spaces[8])){
+      spaces[3] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else if((spaces[1] != ' ') && (spaces[1] == spaces[4]) && (spaces[4] == spaces[7])){
+      spaces[3] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else if((spaces[0] != ' ') && (spaces[0] == spaces[3]) && (spaces[3] == spaces[6])){
+      spaces[3] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else if((spaces[2] != ' ') && (spaces[2] == spaces[5]) && (spaces[5] == spaces[8])){
+      spaces[3] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else if((spaces[0] != ' ') && (spaces[0] == spaces[4]) && (spaces[4] == spaces[8])){
+      spaces[3] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else if((spaces[2] != ' ') && (spaces[2] == spaces[4]) && (spaces[4] == spaces[6])){
+      spaces[3] == player ? cout << "You win\n" : cout << "You win!\n";
+    }
+    else{
+      return false;
+    }
+    return true;
+};
+bool checkTie(char *spaces){
+      for(int i= 0;i < 9;i++){
+        if(spaces[i] == ' '){
+          return false;
+        }
+      }
+      cout << "It is a tie\n";
+
+    return true;
+};
+*/
+
+//X AND O VERSION 3
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player);
+bool checkTie(char *spaces);
+
+int main(){
+    char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' ',};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    drawBoard(spaces);
+
+    while(running){
+        playerMove(spaces, player);
+        drawBoard(spaces);
+        if(checkWinner(spaces, player)){
+            cout << "Player wins!\n";
+            running = false;
+            break;            
+        }
+        else if(checkTie(spaces)){
+            cout << "It's a tie!\n";
+            running = false;
+            break;
+        }
+
+        computerMove(spaces, computer);
+        drawBoard(spaces);
+        if(checkWinner(spaces, computer)){
+            cout << "Computer wins!\n";
+            running = false;
+            break;
+        }
+        else if(checkTie(spaces)){
+            cout << "It's a tie!\n";
+            running = false;
+            break;
+        }
+    }
+    return 0;
+}
+
+void drawBoard(char *spaces){
+    cout << '\n';
+    cout << "     |     |     " << '\n';
+    cout << " " << spaces[0] << "   | " << spaces[1] << "   |  " << spaces[2] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << " " << spaces[3] << "   | " << spaces[4] << "   |  " << spaces[5] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << " " << spaces[6] << "   | " << spaces[7] << "   |  " << spaces[8] << "  " << '\n';
+    cout << "     |     |     " << '\n';
+    cout << '\n';
+}
+
+void playerMove(char *spaces, char player){
+    int number;
+    do{
+        cout << "Enter a spot to place a marker (1-9): ";
+        cin >> number;
+        number--;
+        if(number >= 0 && number < 9 && spaces[number] == ' '){
+            spaces[number] = player;
+            break;
+        } else {
+            cout << "Invalid move. Try again.\n";
+        }
+    } while(true);
+}
+
+void computerMove(char *spaces, char computer){
+    int number;
+    srand(time(NULL));
+
+    while(true){
+        number = rand() % 9;
+        if(spaces[number] == ' '){
+            spaces[number] = computer;
+            break;
+        }
+    }
+}
+
+bool checkWinner(char *spaces, char player){
+    // Checking winning conditions
+    // array[8][3] meaning in the main array we have 8 elements with another 3 elements in them
+    int winningConditions[8][3] = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // rows
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // columns
+        {0, 4, 8}, {2, 4, 6}  // diagonals
+    };
+    for(int i = 0; i < 8; i++){
+        if(spaces[winningConditions[i][0]] == player &&
+           spaces[winningConditions[i][1]] == player &&
+           spaces[winningConditions[i][2]] == player){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool checkTie(char *spaces){
+    for(int i = 0; i < 9; i++){
+        if(spaces[i] == ' '){
+            return false;
+        }
+    }
+    return true;
+}
     }
 }*/
 
